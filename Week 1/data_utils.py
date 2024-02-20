581c3bbb68a03dbcb008a2e311ddd1caeaf13f67
Luis Gonzalez <luis.gonzalez.gudino@estudiantat.upc.edu>
2024-02-20 19:24:41 +0100

    new code utils

---
+++ Week 1/data_utils.py
@@ -0,0 +1,33 @@
+import os
+import torch
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+
+class MIT_split_dataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.classes = sorted(os.listdir(root_dir))
+        self.class_to_idx = {cls_name: idx for idx, cls_name in enumerate(self.classes)}
+        self.images = self._load_images()
+
+    def _load_images(self):
+        images = []
+        for class_name in self.classes:
+            class_dir = os.path.join(self.root_dir, class_name)
+            for img_name in os.listdir(class_dir):
+                img_path = os.path.join(class_dir, img_name)
+                images.append((img_path, self.class_to_idx[class_name]))
+        return images
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_path, label = self.images[idx]
+        img = read_image(img_path).float()
+
+        if self.transform:
+            img = self.transform(img)
+
+        return img, label
